9dc2033fc0e812fa2fc8c695c343bc0a0eeaf936
Kevin Heinicke <kevin@kehei.de>
2015-08-02 01:50:33 +0200

    physicistView offset

--- js/views/physicist.js
+++ js/views/physicist.js
@@ -3,7 +3,9 @@
         // this.graphics.beginFill("red").drawCircle(0, 0, window.cell_size * 0.9 / 2);
         // TODO: read those values from json/whatever
         var width = 131;
+        this.width = width;
         var height = 135;
+        this.height = height;
         var spriteSheet = new createjs.SpriteSheet({
             framerate: 15,
             images: ["img/physicist/physicist-spritesheet.png"],
@@ -23,8 +25,8 @@
     // PhysicistView.prototype = new createjs.Shape();
 
     PhysicistView.prototype.update = function(){
-        this.x = cell_size * this.model.position.x;
-        this.y = cell_size * this.model.position.y;
+        this.x = cell_size * this.model.position.x + cell_size / 2;
+        this.y = cell_size * this.model.position.y + cell_size / 2;
         this.rotation = this.model.direction.x > 0 ? -90 :
             this.model.direction.x < 0 ? 90 :
             this.model.direction.y > 0 ? 0 : 180;
