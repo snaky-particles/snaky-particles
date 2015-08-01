0b992f5bc3450aacc9061825b85bc5f1c942f831
Dima Mironov <okdimok@gmail.com>
2015-08-01 20:07:07 +0200

    spawn collectibles

--- js/models/particle.js
+++ js/models/particle.js
@@ -4,8 +4,10 @@ var Particle = function(position){
 	this.mass = 125;
 	this.charge = 0;
 	this.start_time = 1234;
+	this.decay_time = 4444;
 	this.target = null;
 	this.velocity = null;
 	this.points = 125;
 	this.parent_type = "W";
+	this.decays = [{particles: ["W", "W"], probability: .3 }]
 }
