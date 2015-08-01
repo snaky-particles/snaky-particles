0fe51dc90c8876df8ccbdc3034077084c4e098c6
Dima Mironov <okdimok@gmail.com>
2015-08-01 17:12:48 +0200

    snake moving control

--- models/session.js
+++
@@ -1,7 +0,0 @@
-var Session  = function(){
-	this.size = {x: 20, y: 20}; 
-	this.time_step = 400;
-	this.collectibles = [];
-	this.snakes = [];
-}
-
--- models/snake.js
+++ models/snake.js
@@ -18,3 +18,13 @@ Snake.prototype.add_bonus = function(bonus){
 
 Snake.prototype.remove_physicist = function(index){	
 }
+
+
+Snake.prototype.move = function(next_cell){
+	this.physicist[0].position = next_cell;
+	for(var i = 1; i < this.physicist.length; i++){
+		this.physicists[i].position  = this.physicists[i-1].position;
+		this.physicists[i].direction = this.physicists[i-1].direction;
+	}
+
+}
