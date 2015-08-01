f063d7cf2a6f3de8905ed11d5e704d0fabccc3d6
Dima Mironov <okdimok@gmail.com>
2015-08-01 16:07:10 +0200

    dummy controller

---
+++ controller.js
@@ -0,0 +1,7 @@
+var Controller = function(){
+	
+}
+
+Controller.prototype.start_game = function(){
+	
+}
--- models/snake.js
+++ models/snake.js
@@ -3,6 +3,7 @@ var Snake = function(){
 	for (var i = 0; i < n_physicsists; i++){
 		this.physicists.push_back(new Physicist(this, position));
 	}
+	this.new_physicists = [];
 	this.bonuses = [];
 	this.speed = 1;
 }
