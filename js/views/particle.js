a36e70aed813e9cfc878c14047ebfded990cd633
Kevin Heinicke <kevin@kehei.de>
2015-08-01 23:23:26 +0200

    we have moving particles!

--- js/views/particle.js
+++ js/views/particle.js
@@ -11,5 +11,21 @@
         this.y = cell_size * this.model.position.y;
     }
 
+    ParticleView.prototype.animate = function(){
+        if(this.model.target){
+            var own_view = this;
+            this.update();
+            this.update = function(){};
+            createjs.Tween.get(this).to(
+                { x: cell_size * this.model.target.x, y: cell_size * this.model.target.y },
+                this.model.target.time - this.model.start_time
+            ).call(function(){
+                own_view.update = ParticleView.prototype.update;
+                own_view.model.position.x = own_view.model.target.x;
+                own_view.model.position.y = own_view.model.target.y;
+            });
+        }
+    }
+
     window.ParticleView = ParticleView;
 }(window));
