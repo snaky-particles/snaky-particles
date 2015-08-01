2ed65f37b2b3b4b752346165b71496680e8daf29
eraisedtoipi <peerutpeet@hotmail.com>
2015-08-01 15:47:34 +0200

    ph models

---
+++ models/physicist.js
@@ -0,0 +1,11 @@
+var Physicist = function(snake, position) {
+	this.direction = 0;
+	this.position = position;
+	this.name = 'Fermi';
+	this.bonus = '';
+	this.snake = snake;
+};
+
+Physicist.prototype.catch = function(collectible) {
+
+};
